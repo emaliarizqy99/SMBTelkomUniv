2ee26d60835df92a54e5a74a4e236f04a14056e1
emaliarizqy99 <emooll30@gmail.com>
2016-10-14 12:51:08 +0700

    start

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4012/smbtelkomuniv/MainActivity.java
@@ -0,0 +1,22 @@
+package id.sch.smktelkom_mlg.tugas01.xiirpl4012.smbtelkomuniv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvHasil;
+    Button bOk;
+    EditText etNM, etTH, etTP;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
